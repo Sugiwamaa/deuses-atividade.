087ba1a31e20f4cb82ada0dd268abea47b2c0e00
Sugiwamaa <106767936+Sugiwamaa@users.noreply.github.com>
2022-10-26 17:07:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder'] Adicione os 3 Deuses: ('Loki', 'Odin', 'Frey') com a função push()
+
+let deuses = ['Aegir', 'Aud', 'Balder'];
+
+deuses.push('Loki', 'Odin', 'Frey');
+console.log(deuses);
+
+//Crie o seguinte array: var numbers = [5, 7, 1, 8, 9] E exiba de forma crescente dica: .sort
+
+let number = [5,7,1,8,9]
+number.sort()
+
+console.log(number)
+
+number.reverse()
+console.log(number)
+
+//Crie o seguinte array: var numbers = [5, 7, 1, 8, 9] E exiba de forma crescente dica: .sort 
+
+
+
+    
